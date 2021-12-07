a498bcfbdae4657633676bf022645ab049e83688
Naveen M <naveenmnka15@gmail.com>
2021-12-07 12:40:13 +0530

    b

--- res/script/main.js
+++ res/script/main.js
@@ -1,21 +1,4 @@
-function pass(){
-    var passcode = document.getElementById("password").value;
-    if (passcode=="n9751574651"){
-        window.location.href = "/res/html/sample.html";
-        window.alert("success!");
-    }
-    else{
-        document.getElementById("alert").innerHTML = "Who are You?"
-        Email.send({
-            Host: "smtp.gmail.com",
-            Username : "www.naveenmnka9655@gmail.com",
-            Password : "n9751574651",
-            To : 'www.naveenmnka15@gmail.com',
-            From : "Naveen's Portal",
-            Subject : "Who one tried to login",
-            Body : "Alert!",
-            }).then(
-                message => alert("mail sent successfully")
-            );
-    }
+window.alert("hello")
+function click(){
+    window.alert("hello");
 }
